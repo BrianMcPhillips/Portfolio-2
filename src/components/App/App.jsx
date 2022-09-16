22b055027eb246be6543bc052dab8810a5135146
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-16 16:30:40 -0400

    About component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../About/About';
 import Intro from '../Intro/Intro';
 
 const App = () => {
   return (
     <div>
       <Intro />
+      <About />
     </div>
   );
 }
