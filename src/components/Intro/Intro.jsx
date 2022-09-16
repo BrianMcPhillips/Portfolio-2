0ccd2537d55a0b12424b84eab6bba7a74859b41d
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-16 11:54:11 -0400

    Intro text

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -9,6 +9,15 @@ const Intro = () => {
         <div className='i-left-wrapper'>
           <h2 className='i-intro'>Hello, My name is</h2>
           <h1 className='i-name'>Brian McPhillips</h1>
+          <div className="i-title">
+            <div className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Audio Engineer</div>
+              <div className="i-title-item">Disc Golfer</div>
+              <div className="i-title-item">Watch Enthusiast</div>
+              <div className="i-title-item">Something</div>
+            </div>
+          </div>
         </div>
       </div>
       <div className='i-right'>right</div>
