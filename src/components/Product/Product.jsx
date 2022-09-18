cdbf8e0b165728944794bd631b82da078d233d11
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-18 14:16:59 -0400

    Cards finished with styling

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,13 +5,13 @@ const Product = () => {
 
   return (
     <div className='p'>
-      <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
+      <div className='p-browser'>
+        <div className='p-circle'></div>
+        <div className='p-circle'></div>
+        <div className='p-circle'></div>
       </div>
-      <a href="https://www.placecage.com" target='_blank' rel='noreferrer'>
-        <img src="https://www.placecage.com/300/300" alt="cage" />
+      <a href='https://www.placecage.com' target='_blank' rel='noreferrer'>
+        <img className='p-img' src='https://www.placecage.com/200/400' alt='cage' />
       </a>
     </div>
   );
