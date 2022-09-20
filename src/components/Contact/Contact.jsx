ef293d4addf975c1a9675c713f7a619c6ee957e5
Brian McPhillips <B.McPhillips89@gmail.com>
2022-09-19 20:21:38 -0400

    Contact info and icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
+import gmail from '../../images/gmail.png';
+import linkedin from '../../images/linkedin.png';
+import github from '../../images/github.png';
 
 const Contact = () => {
 
   return (
     <div className='c'>
       <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss things</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img 
+                src={gmail} 
+                alt="Brian McPhillips Email" 
+                className='c-icon'
+              />
+              BMcPhillipsDev@gmail.com
+            </div>
+            <div className="c-info-item">
+              <img 
+                src={linkedin} 
+                alt="Brian McPhillips Linked-in page" 
+                className='c-icon'
+              />
+              https://Linkedin.com/in/BrianMcPhillips
+            </div>
+            <div className="c-info-item">
+              <img 
+                src={github} 
+                alt="Brian McPhillips github page" 
+                className='c-icon'
+              />
+              https://github.com/BrianMcPhillips
+            </div>
+          </div>
+        </div>
+        <div className="c-right"></div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Contact;
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -6,9 +6,9 @@ const Product = () => {
   return (
     <div className='p'>
       <div className='p-browser'>
-        <div className='p-circle'></div>
-        <div className='p-circle'></div>
-        <div className='p-circle'></div>
+        <div className='p-circle-i'></div>
+        <div className='p-circle-ii'></div>
+        <div className='p-circle-iii'></div>
       </div>
       <a href='https://www.placecage.com' target='_blank' rel='noreferrer'>
         <img className='p-img' src='https://www.placecage.com/200/400' alt='cage' />
